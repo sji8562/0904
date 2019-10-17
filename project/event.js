9dd3f5dcd476aaa544d60223e74c7ce410f915d2
sji8562 <whaqlzkem@naver.com>
2019-10-18 01:01:44 +0900

    project

---
+++ project/event.js
@@ -0,0 +1,363 @@
+var sound_hit = new Audio('../sounds/hit.mp3');
+var sound_collision = new Audio('../sounds/collision.mp3');
+var gauge = 0;
+var one = 0.1;
+var id;
+var power = 0;
+var tempX;
+var tempY;
+var TimerID;
+var i = 0;
+var isfirst = true;
+var guide_x;
+var guide_y;
+//마우스를 현재 좌표를 받기위한 오브젝트 좌표
+function findOffset(obj) {
+    var curX = 0;
+    var curY = 0;
+    if (obj.offsetParent) {
+        do {
+            curX += obj.offsetLeft;
+            curY += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return {x: curX, y: curY};
+    }
+}
+//캔버스 수정을 위한 펑션
+function updateCanvas(e) {
+    //canvas좌표를 pos에 가져옴
+    var pos = findOffset(canvas);
+
+    mouseX = e.pageX - pos.x;   // 마우스의 현재 좌표
+    mouseY = e.pageY - pos.y;
+
+    nowdegree = 180 * Math.atan2(mouseX - cue.x, mouseY - cue.y) / Math.PI;
+
+    if (cue.mouse && !cue.drag) {
+        canvas.style.cursor = cursor_grab;
+    } else if (cue.drag) {
+        canvas.style.cursor = cursor_drag;
+    } else {
+        canvas.style.cursor = 'auto';
+    }
+    if (cue.drag) {
+        cue.degree = -(nowdegree - 90);
+    }
+
+    draw_cue();
+}
+
+function startDrag() {
+    if (cue.mouse == true) {
+        cue.drag = true;
+    }
+}
+
+function stopDrag() {
+    one = 0.1;
+    if (cue.drag == true) {
+        cue.drag = false;
+    }
+}
+
+function frame() {
+    one = one + 0.007;
+    gauge += one;
+    if (gauge >= 100.0) {
+        one = -one;
+    }
+
+    if (gauge <= 0) {
+        one = 0.1;
+        one = Math.abs(one);
+    }
+    elem.style.width = gauge + '%';
+    document.getElementById("label").innerHTML = gauge.toFixed(1) + '%';
+}
+//게이지 채우기
+function startGauge() {
+    id = setInterval(frame, 10);
+    waitkey = false;
+}
+//게이지 멈추고 공을 친다
+function stopGauge() {
+    clearInterval(id);
+    power = gauge;
+    gauge = 0;
+    elem.style.width = gauge + '%';
+    document.getElementById("label").innerHTML = gauge.toFixed(1) + '%';
+    hit.disabled = true;
+    cue_execute();
+    waitkey = true;
+}
+
+function cue_motion() {
+    i++;
+    cue.x = cue.x + i * Math.cos(degreeToRadian * cue.degree);
+    cue.y = cue.y + i * Math.sin(degreeToRadian * cue.degree);
+
+    draw_cue();
+    if (i >= 100) {
+        clearInterval(TimerID);
+        cue.x = tempX - 20 * Math.cos(degreeToRadian * cue.degree);
+        cue.y = tempY - 20 * Math.sin(degreeToRadian * cue.degree);
+        draw_cue();
+        sound_hit.play();
+        i = 0;
+        // setTimeout(function () {
+        cue.visible = false;
+        HitBall();
+        draw();
+        // }, 500);
+
+    }
+
+    cue.x = tempX;
+    cue.y = tempY;
+}
+//큐 움직임
+function cue_execute() {
+
+    tempX = cue.x;
+    tempY = cue.y;
+
+    TimerID = setInterval(cue_motion, 10);
+}
+//가이드 점선
+function draw_guide_1() {
+    var gx = balls[nowPlayer].x + 60;
+    var gy = balls[nowPlayer].y + 60;
+
+    var degree = cue.degree * degreeToRadian;
+
+    var point_x = gx - 20 * Math.cos(degree);
+    var point_y = gy - 20 * Math.sin(degree);
+
+    //왼쪽 쿠션
+
+    var guide_left_x = 60;
+    var guide_left_y = gy - Math.tan(degreeToRadian * cue.degree) * (point_x - 60);
+
+    //오른쪽 쿠션
+    var guide_right_x = 1140;
+    var guide_right_y = gy - Math.tan(degreeToRadian * cue.degree) * (point_x - 1140);
+
+    //상단 쿠션
+    var guide_top_x = gx - Math.tan(degreeToRadian * (90 - cue.degree)) * (point_y - 60);
+    var guide_top_y = 60;
+
+    //하단 쿠션
+    var guide_bottom_x = gx - Math.tan(degreeToRadian * (90 - cue.degree)) * (point_y - 610);
+    var guide_bottom_y = 610;
+
+
+    if (isfirst) {
+        guide_x = guide_left_x;
+        guide_y = guide_left_y;
+    }
+
+    //가이드라인 각도조절
+
+    //왼쪽상단부터 오른쪽상단까지
+    if (guide_y <= 60 && (guide_x > 60 || guide_x <= 1140)) {
+        guide_x = guide_top_x; //가변값
+        guide_y = guide_top_y; //610
+        isfirst = false;
+    }
+
+    //오른쪽 상단부터 오른쪽 하단까지
+    if (guide_x >= 1140 && (guide_y > 60 || guide_y <= 610)) {
+        guide_x = guide_right_x; // 1140
+        guide_y = guide_right_y; // 가변값
+    }
+
+    //오른쪽 하단부터 왼쪽 하단까지
+    if (guide_y >= 610 && (guide_x >= 60 || guide_x < 1140)) {
+        guide_x = guide_bottom_x; //가변값
+        guide_y = guide_bottom_y; //610
+    }
+
+    //왼쪽하단에서 왼쪽 상단까지
+    if (guide_x <= 60 && (guide_y >= 60 || guide_y < 610)) {
+        guide_x = guide_left_x; //60
+        guide_y = guide_left_y; //가변값
+        isfirst = false;
+    }
+
+//점선그리기
+    ctx.setLineDash([5, 10]);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.moveTo(point_x, point_y);
+    ctx.lineTo(guide_x, guide_y);
+    ctx.stroke();
+    ctx.closePath();
+}
+//선수 교체
+function playerChange() {
+    p1 = document.getElementById("p1");
+    p2 = document.getElementById("p2");
+    nowPlayer++;
+    nowPlayer = nowPlayer % 2;
+
+    if (nowPlayer == 0) {
+        p2.style.color = "lightgrey";
+        p1.style.color = "dodgerblue";
+    }
+    else {
+        p2.style.color = "dodgerblue";
+        p1.style.color = "lightgrey";
+    }
+    p1.innerHTML = "Player1 : " + scoreinfo[0];
+    p2.innerHTML = "Player2 : " + scoreinfo[1];
+
+    draw();
+}
+
+var waitkey = true;
+var waite = true;
+
+function keyEvent1(e) {
+    player.disabled = true;
+    if (waitkey) {
+        if (e.keyCode === 38) { //위키
+            cue.degree--;
+        }
+        else if (e.keyCode === 40) { //아래키
+            cue.degree++;
+        }
+
+        else if (e.keyCode === 37) { //왼쪽키
+            cue.degree -= 3;
+        }
+
+
+        else if (e.keyCode === 39) { //오른쪽키
+            cue.degree += 3;
+        }
+
+
+        else if (e.keyCode === 67) { //c버튼
+            if (nowPlayer == 0) {
+                p2.style.color = "lightgrey";
+                p1.style.color = "dodgerblue";
+            }
+            else {
+                p2.style.color = "dodgerblue";
+                p1.style.color = "lightgrey";
+            }
+            p1.innerHTML = "Player1 : " + scoreinfo[0];
+            p2.innerHTML = "Player2 : " + scoreinfo[1];
+            playerChange();
+        }
+
+        else if (e.keyCode === 32 && waite) { //w버튼
+            startGauge();
+            waite = false;
+        }
+        draw();
+    }
+}
+
+function keyEvent2(e) {
+    if (e.keyCode === 32 && waite) { //w버튼
+        stopGauge();
+        waite = false;
+        waitkey = true;
+    }
+}
+
+function getscore() {
+    var text;
+    var turnover = false;
+    if (!balls[nowPlayer].loss) //적구를 맞지 않았을때
+    {
+        if (balls[nowPlayer].red1 && balls[nowPlayer].red2) {
+            scoreinfo[nowPlayer]++; //득점
+            text = "Nice Shoot!";
+        }
+
+        else if (balls[nowPlayer].red1 || balls[nowPlayer].red2) {
+            //무실점 무득점
+            text = "Let's do better!";
+            turnover = true;
+        }
+
+
+        else {
+            text = "Oh My God !!"; //실점
+            if (scoreinfo[nowPlayer] > 0)
+                scoreinfo[nowPlayer]--;
+            turnover = true;
+        }
+    }
+    else {
+        text = "Oh My God !!"; //실점
+        if (scoreinfo[nowPlayer] > 0)
+            scoreinfo[nowPlayer]--;
+        turnover = true;
+    }   //오류나는 이유 나우공이 바뀌기전에 공이 맞은정보가 초기화 되야함
+    //또한 메시지도 즉시 출력됨
+
+    balls[nowPlayer].red1 = false;  //공이 맞은 정보 초기화
+    balls[nowPlayer].red2 = false;
+    balls[nowPlayer].loss = false;
+
+    if (turnover) {
+        nowPlayer = ++nowPlayer % 2;// 차례변경
+        cue.x = balls[nowPlayer].x + 60;
+        cue.y = balls[nowPlayer].y + 60;
+    }
+    p1 = document.getElementById("p1");
+    p2 = document.getElementById("p2");
+    //위 Player1,2색깔변경
+    //본인차례일때 파란색
+    if (nowPlayer == 0) {
+        p2.style.color = "lightgrey";
+        p1.style.color = "dodgerblue";
+    }
+    else {
+        p2.style.color = "dodgerblue";
+        p1.style.color = "lightgrey";
+    }
+    //플레이어 점수
+    p1.innerHTML = "Player1 : " + scoreinfo[0];
+    p2.innerHTML = "Player2 : " + scoreinfo[1];
+
+    //영역지우기
+    ctx.clearRect(0, 0, w, h);
+    //다시 불러옴
+    table.draw();
+    //공을 쳤을때 좋다, 더 잘하자 , 좋지않다 폰트
+    setTimeout(function () {
+        ctx.font = 'italic 100px calibri';
+        ctx.fillText(text, 250, 250);
+    }, 500)
+
+
+}
+//도움말 내용
+function help_alert() {
+    swal(
+
+        "도움말",
+        "룰\n" +
+        "1. 한번 움직일때 빨간공 두개를 맞춰라\n"+
+        "2. 하얀공을 맞으면 실점\n"+
+        "3. 공을 하나도 맞추지 못할경우 실점\n\n"+
+
+        "마우스로 게임할때\n"+
+        "1. 큐를 드래그해서 각도를 맞춘다\n" +
+        "2. 공을친다 버튼을 눌러 힘을 조절할수있다\n\n" +
+        "3. 버튼을 놓으면 큐로 치면서 공이 나아간다"+
+
+        "키보드로 겡미할때\n"+
+        "1. 왼,오른쪽 버튼(←, →) : 큰 각도 조절 \n" +
+        "2. 위,아래 버튼 (↑, ↓) : 작은 각도 조절\n" +
+        "3. 스페이스바 : 공을친다 버튼을 눌렀을때와 같음\n" +
+        "4. C버튼 : 선수교체 버튼을 눌렀을때와 같음\n"+
+        "5. W버튼 : 공이 빨리 움직여 시간을 단축한다",
+
+        "info"
+    );
+}
---
+++ project/example.js
@@ -0,0 +1,470 @@
+//html에  id들을 불러옴
+var help = document.getElementById("help_btn");
+var hit = document.getElementById("hit_btn");
+var player = document.getElementById("player_change");
+var elem = document.getElementById("myBar");
+//드래그 와 드롭을 하기 위한 마우스 현재좌표
+var mouseX = 0, mouseY = 0;
+var nowdegree = 0;
+var cursor_grab = "url(DATA URI), move";
+var cursor_drag = "url(DATA URI), move";
+var degreeToRadian = Math.PI / 180;
+//html dom 트리에 캔버스 요소 추가
+var canvas = document.querySelector('canvas'),
+    //캔버스에 2d로 그릴 수 있도록 준비
+    ctx = canvas.getContext('2d'),
+    w = window.innerWidth,
+    h = window.innerHeight,
+    points = 4,
+    colors = ['#feca28', '#f8f6ea', '#e51515', '#e51515'],
+    balls = [],
+    table,
+    mouse = {
+        down: false,
+        button: 1,
+        x: 0,
+        y: 0,
+        px: 0,
+        py: 0
+    },
+    elasticity = .8,
+    refreshHz = 60,
+    velocityCutoff = 0.01,
+    bounceLoss = .85,
+    cue,
+    tableFriction = 0.00003;
+canvas.width = w;
+canvas.height = h;
+var power = 0;
+xlocations = [180, 20, 200, 240],
+    ylocations = [400, 275, 200, 240];
+nowPlayer = 0;
+shootend = false;
+var scoreinfo = [0, 0];
+
+//리페인트 이전에 실행할 콜백을 받습니다
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+        //1초
+            window.setTimeout(callback, 1000 / refreshHz);
+        };
+})();
+//window가 로드 되면
+window.onload = function () {
+
+    canvas.addEventListener('mousemove', updateCanvas, false);    //움직일때
+    canvas.addEventListener('mousedown', startDrag, false); // 버튼을 누를때
+    canvas.addEventListener('mouseup', stopDrag, false);  // 버튼을 놓을때
+
+    player.addEventListener('click', playerChange, false); //선수교체 누를때
+    hit.addEventListener('mousedown', startGauge, false); //공을치다 누를때
+    hit.addEventListener('mouseup', stopGauge, false); //공을치다 땟을때
+    help.addEventListener('mousedown', help_alert, false);//도움말 누를때
+
+    document.addEventListener("keydown", keyEvent1); //
+    document.addEventListener("keyup", keyEvent2);
+
+    draw();
+};
+//큐대
+var Cue = function (ball) {
+    this.x = ball.x + 60;
+    this.y = ball.y + 60;
+    this.degree = 0;
+    this.mouse = false;
+    this.drag = false;
+    this.visible = true;
+}
+
+function draw_cue() {
+
+    if (cue.visible) {
+        ctx.clearRect(0, 0, w, h);
+        table.draw();
+        for (var i = 0; i < points; i++) {
+            var temp = balls[i];
+            temp.TestImpact();
+            temp.Update(table);
+            temp.draw(table);
+        }
+
+        var degree = cue.degree * degreeToRadian;
+        var x1_start = cue.x + 40 * Math.cos(degree + degreeToRadian * 5);
+        var x2_start = cue.x + 40 * Math.cos(degree - degreeToRadian * 5);
+        var x3_start = cue.x + 50 * Math.cos(degree + degreeToRadian * 5);
+        var x4_start = cue.x + 50 * Math.cos(degree - degreeToRadian * 5);
+        var y1_start = cue.y + 40 * Math.sin(degree + degreeToRadian * 5);
+        var y2_start = cue.y + 40 * Math.sin(degree - degreeToRadian * 5);
+        var y3_start = cue.y + 50 * Math.sin(degree + degreeToRadian * 5);
+        var y4_start = cue.y + 50 * Math.sin(degree - degreeToRadian * 5);
+
+
+        ctx.beginPath();
+        ctx.moveTo(x1_start, y1_start);
+        ctx.lineTo(x2_start, y2_start);
+        ctx.lineTo(x4_start, y4_start);
+        ctx.lineTo(x3_start, y3_start);
+        ctx.fillStyle = "#f8f6ea";
+        ctx.fill();
+
+        var x1_end = cue.x + 700 * Math.cos(degree + degreeToRadian * 0.6);
+        var x2_end = cue.x + 700 * Math.cos(degree - degreeToRadian * 0.6);
+        var y1_end = cue.y + 700 * Math.sin(degree + degreeToRadian * 0.6);
+        var y2_end = cue.y + 700 * Math.sin(degree - degreeToRadian * 0.6);
+
+
+        ctx.fillStyle = "#f6dfbd";
+        ctx.beginPath();
+        ctx.save();
+        ctx.shadowOffsetX = 10;
+        ctx.shadowOffsetY = 10;
+        ctx.shadowColor = "rgba(0,0,0,.3)";
+        ctx.shadowBlur = 5;
+        ctx.moveTo(x1_start, y1_start);
+        ctx.lineTo(x2_start, y2_start);
+        ctx.lineTo(x2_end, y2_end);
+        ctx.lineTo(x1_end, y1_end);
+        ctx.isPointInPath(mouseX, mouseY) ? cue.mouse = true : cue.mouse = false;  //현재 경로에 포함되있는지 확인
+        ctx.fill();
+
+        var x1_middle = cue.x + 520 * Math.cos(degree + degreeToRadian * 0.6);
+        var x2_middle = cue.x + 520 * Math.cos(degree - degreeToRadian * 0.6);
+        var x3_middle = cue.x + 450 * Math.cos(degree);
+        var x4_middle = cue.x + 680 * Math.cos(degree + degreeToRadian * 0.6);
+        var x5_middle = cue.x + 680 * Math.cos(degree - degreeToRadian * 0.6);
+
+        var y1_middle = cue.y + 520 * Math.sin(degree + degreeToRadian * 0.6);
+        var y2_middle = cue.y + 520 * Math.sin(degree - degreeToRadian * 0.6);
+        var y3_middle = cue.y + 450 * Math.sin(degree);
+        var y4_middle = cue.y + 680 * Math.sin(degree + degreeToRadian * 0.6);
+        var y5_middle = cue.y + 680 * Math.sin(degree - degreeToRadian * 0.6);
+
+        ctx.fillStyle = "#1a1a18";
+
+        ctx.beginPath();
+        ctx.moveTo(x1_middle, y1_middle);
+        ctx.lineTo(x3_middle, y3_middle);
+        ctx.lineTo(x2_middle, y2_middle);
+        ctx.lineTo(x5_middle, y5_middle);
+        ctx.lineTo(x4_middle, y4_middle);
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.restore();
+        draw_guide_1();
+
+    }
+}
+
+
+var Table = function () {
+    this.xPos = 60;
+    this.yPos = 60;
+    this.width = 1080;
+    this.height = 550;
+}
+
+
+Table.prototype.draw = function () {
+    var tw = this.width + 120;
+    var th = this.height + 120;
+
+    ctx.setLineDash([]);
+    ctx.fillStyle = "#6a5746";
+    ctx.fillRect(0, 0, tw, th);
+    ctx.fillStyle = "#3456af";
+    ctx.fillRect(45, 45, tw - 90, th - 90);
+    ctx.fillStyle = "#4370d7";
+    ctx.fillRect(60, 60, tw - 120, th - 120);
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(60, 60);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(tw, 0);
+    ctx.lineTo(tw - 60, 60);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(0, th);
+    ctx.lineTo(60, th - 60);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(tw, th);
+    ctx.lineTo(tw - 60, th - 60);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.fillStyle = "#b0b0b0";
+
+    for (var i = 1; i <= 7; i++) {
+        ctx.beginPath();
+//arc(중심점x, 중심점y, 반지름, 시작각도, 끝각도, 방향)
+//true : 반시계 방향 , false : 시계 방향
+        ctx.arc(60 + (tw - 120) / 8 * i, 30, 5, 0, Math.PI * 2, true);
+        ctx.strokeStyle = "#0";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(60 + (tw - 120) / 8 * i, th - 30, 5, 0, Math.PI * 2, true);
+        ctx.strokeStyle = "#0";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    for (var i = 1; i <= 3; i++) {
+        ctx.beginPath();
+        ctx.arc(30, 60 + (th - 120) / 4 * i, 5, 0, Math.PI * 2, true);
+        ctx.strokeStyle = "#0";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(tw - 30, 60 + (th - 120) / 4 * i, 5, 0, Math.PI * 2, true);
+        ctx.strokeStyle = "#0";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+
+var Ball = function (i) {
+    this.r = 20;
+    this.x = xlocations[i % points];
+    this.y = ylocations[i % points];
+    this.opacity = 1;
+    this.xVelocity = 0;
+    this.yVelocity = 0;
+    this.xAccel = 0;
+    this.yAccel = 0;
+    this.bounceLoss = bounceLoss;
+    this.tableFriction = tableFriction;
+    this.c = colors[i % points];
+    this.index = i;
+    // this.move = false;
+    this.red1 = false;
+    this.red2 = false;
+    this.loss = false;
+}
+
+Ball.prototype.draw = function (table) {
+    ctx.fillStyle = this.c;
+    ctx.globalAlpha = this.opacity;
+    ctx.beginPath();
+    ctx.save();
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowColor = "rgba(0,0,0,.3)";
+    ctx.shadowBlur = 5;
+    ctx.arc(this.x + table.xPos,
+        this.y + table.yPos,
+        this.r, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+};
+
+Ball.prototype.Update = function (table) {
+    var dT = 1000 / refreshHz;
+    this.xAccel = this.xVelocity * -this.tableFriction * dT;
+    this.yAccel = this.yVelocity * -this.tableFriction * dT;
+    this.yVelocity += this.yAccel * dT;
+    this.xVelocity += this.xAccel * dT;
+    this.y += this.yVelocity * dT;
+    this.x += this.xVelocity * dT;
+
+    var bounce = false;
+    if (this.y >= table.height - this.r) // Ball at bottom edge
+    {
+        this.y = table.height - this.r;
+        this.yVelocity = -this.yVelocity;
+        this.yAccel = -this.yAccel;
+        bounce = true;
+    }
+    else if (this.y <= this.r) // Ball at top edge
+    {
+        this.y = this.r;
+        this.yVelocity = -this.yVelocity;
+        this.yAccel = -this.yAccel;
+        bounce = true;
+    }
+
+    if (this.x >= table.width - this.r) //  오른쪽 끝에 있는 공
+    {
+        // 겹치지 않도록
+        this.x = table.width - this.r;
+        // "Bounce" it
+        this.xVelocity = -this.xVelocity;
+        this.xAccel = -this.xAccel;
+        bounce = true;
+    }
+    else if (this.x <= this.r) // 왼쪽 끝에 있는 공
+    {
+        this.x = this.r;
+        this.xVelocity = -this.xVelocity;
+        this.xAccel = -this.xAccel;
+        bounce = true;
+    }
+
+    // Update velocity
+    if (bounce) {
+        this.xVelocity *= this.bounceLoss;
+        this.yVelocity *= this.bounceLoss;
+    }
+
+    if (Math.abs(this.xVelocity) + Math.abs(this.yVelocity) < velocityCutoff) {
+        this.yVelocity = 0;
+        this.yAccel = 0;
+        this.xVelocity = 0;
+        this.xAccel = 0;
+    }
+
+};
+
+Ball.prototype.Strike = function (xImpact, yImpact) {
+    this.xVelocity += xImpact;
+    this.yVelocity += yImpact;
+};
+
+function CollideBalls(ball, ball2) {
+    // 충돌 감지
+
+    var lossball;
+    sound_collision.play();
+
+    lossball = (nowPlayer + 1) % 2;
+
+    if (ball == balls[nowPlayer]) {
+        if (ball2 == balls[lossball])
+            balls[nowPlayer].loss = true;
+        if (ball2 == balls[2])
+            balls[nowPlayer].red1 = true;
+        if (ball2 == balls[3])
+            balls[nowPlayer].red2 = true;
+    }
+
+    var Del = ball2.r + ball.r;
+    var dX = ball2.x - ball.x;
+    var dY = ball2.y - ball.y;
+    var dVX = ball2.xVelocity - ball.xVelocity;
+    var dVY = ball2.yVelocity - ball.yVelocity;
+    var dSq = dX * dX + dY * dY;
+    var alpha = (1 + elasticity) / 2 * (dX * dVX + dY * dVY) / dSq;
+
+    //충돌했을때 충돌한 공이 나아 갈수 있도록함
+    ball.xVelocity += dX * alpha;
+    ball.yVelocity += dY * alpha;
+    ball2.xVelocity -= dX * alpha;
+    ball2.yVelocity -= dY * alpha;
+    //충돌했을때 공이 맞은 각도로 꺽일수 있게 함
+    var DDist = ((Del + 1) / Math.sqrt(dSq) - 1) / 2;
+    ball.x -= dX * DDist;
+    ball.y -= dY * DDist;
+    ball2.x += dX * DDist;
+    ball2.y += dY * DDist;
+
+}
+
+Ball.prototype.TestImpact = function () {
+    for (var i = this.index + 1; i < points; i++) {
+        var ball = balls[i];
+        if (Dist(this.x, this.y, ball.x, ball.y) > this.r + ball.r) {
+            continue;
+        }
+        CollideBalls(this, ball);
+    }
+}
+
+//공을쳤을때 움직이는 공식
+function HitBall() {
+    d_power = power * 0.75;
+
+    var mouseDownX = cue.x - d_power * Math.cos(cue.degree * degreeToRadian);
+    var mouseDownY = cue.y - d_power * Math.sin(cue.degree * degreeToRadian);
+
+    var dX = mouseDownX - balls[nowPlayer].x - 60;
+    var dY = mouseDownY - balls[nowPlayer].y - 60;
+    shootend = true;
+    balls[nowPlayer].Strike(dX / 50.0, dY / 50.0);
+
+}
+
+function Dist(x1, y1, x2, y2) {
+    var diffX = x2 - x1;
+    var diffY = y2 - y1;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+}
+
+
+(function init() {
+
+        for (var i = 0; i < points; i++) {
+            balls.push(new Ball(i));
+        }
+
+        for (var i = this.index + 1; i < points; i++) {
+            var ball = balls[i];
+            if (Dist(this.x, this.y, ball.x, ball.y) > this.r + ball.r) {
+                continue;
+            }
+        }
+
+        table = new Table();
+        cue = new Cue(balls[nowPlayer]);
+    }
+)();
+
+
+function draw() {
+    var stop = true;
+    ctx.clearRect(0, 0, w, h);
+    table.draw();
+    for (var i = 0; i < points; i++) {
+        var temp = balls[i];
+        temp.TestImpact();
+        temp.Update(table);
+        temp.draw(table);
+    }
+
+    for (var i = 0; i < points; i++) {
+        var temp = balls[i];
+        stop = stop && (temp.xVelocity == 0 && temp.yVelocity == 0)
+    }
+
+
+    if (stop) {//공이 모두 멈췄을때
+        cue.x = balls[nowPlayer].x + 60;
+        cue.y = balls[nowPlayer].y + 60;
+        cue.visible = true;
+        hit.disabled = false;
+        player.disabled = false;
+        waite = true;
+        stop = !stop;
+
+        if (shootend) {
+            getscore();
+            shootend = false;
+        }
+    }
+
+    else {// 공이 움직일때
+        hit.disabled = true;
+        player.disabled = true;
+    }
+
+    draw_cue();
+
+    if (!cue.visible) {
+        requestAnimFrame(draw);
+    }
+}
